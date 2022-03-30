03435adf5cc11540f4133d35fc671b057a29244c
springleeo <springleeo@163.com>
2022-03-30 22:41:17 +0800

    课时84【用户模块】登录时间、登录IP的更新及api文档的优化

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
             <i v-if="!collapse" class="el-icon-s-fold"></i>
             <i v-else class="el-icon-s-unfold"></i>
         </div>
-        <div class="logo">知了网盘共享系统</div>
+        <div class="logo">网盘共享系统</div>
         <div class="header-right">
             <div class="header-user-con">
                 <!-- 全屏显示 -->
